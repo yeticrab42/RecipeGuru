2955fde1b83b8b733d21bc81ca47c9c6a444dc6a
JeremyHcWang <haocheng.9758@gmail.com>
2023-08-20 14:17:01 -0400

    minor update

--- client/components/Card.js
+++ client/components/Card.js
@@ -1,18 +1,27 @@
 import React from "react";
 
-const Card = ({image, title, usedIngredients}) => {
-    // const {image, title, usedIngredients} = props
-    let arr = []
+const Card = ({image, title, usedIngredients, missedIngredients}) => {
+    let arr = [], arr2 = []
      usedIngredients.forEach(el => {
-        arr.push(<p key={el}>{el}</p>)
+        arr.push(<li key={el}>{el}</li>)
     })
+    missedIngredients.forEach(el => {
+      arr2.push(<li key={el}>{el}</li>)
+  })
     return (
+      
         <div style={styles.container}>
             <img src={image}></img>
             <p>{title}</p>
             <p>Used ingredients:</p>
-                
+            <ul>
             {arr}
+            </ul>
+            <p>Missed ingredients:</p>
+            <ul>
+            {arr2}
+            </ul>
+            <input type="submit" name="addToFavorites" value="Add To Favorites"></input>
             <br/>
         </div>
     )
@@ -20,11 +29,15 @@ const Card = ({image, title, usedIngredients}) => {
   
   const styles = {
     container: {
+      // display: 'flex',
+      // wrap: 'wrap',
       border: '1px solid black',
-      height: '50%',
-      width: '50%',
-      flex: 1,
+      // height: '50%',
+      // width: '50%',
     },
+    body: {
+      
+    }
   };
 
 export default Card;
