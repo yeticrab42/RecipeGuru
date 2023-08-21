3185a9374b0edff276b8a606e44dd89398015aa2
Tiffany Wong <wongt1227@gmail.com>
2023-08-21 12:26:27 -0400

    +deleting favorites

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -17,17 +17,35 @@ userController.addUser = async (req, res, next) => {
   } catch (error) {
     return next(error);
   }
-  //   User.create({
-  //     username,
-  //     password,
-  //   })
-  //     .then((createdUser) => {
-  //       console.log(createdUser);
-  //       return next();
-  //     })
-  //     .catch((err) => {
-  //       return next(err);
-  //     });
+};
+
+userController.removeFavorite = async (req, res, next) => {
+  console.log('inside userController.removeFavorite');
+  const { image, title, usedIngredients, missedIngredients } = req.body;
+
+  try {
+    const foundItem = await Favorites.findOne({
+      image,
+      title,
+      usedIngredients,
+      missedIngredients,
+    });
+
+    const arr = [];
+    const user = await User.findOne({ username: 'user1' });
+    for (let i = 0; i < user.favorited.length; i++) {
+      if (user.favorited[i].toString() === foundItem._id.toString()) {
+        console.log(i);
+      } else {
+        arr.push(user.favorited[i]);
+      }
+    }
+    await User.updateOne({ username: 'user1' }, { favorited: arr });
+
+    return next();
+  } catch (error) {
+    return next(error);
+  }
 };
 
 module.exports = userController;
